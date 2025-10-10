ae6ce8cd05e2beef7c13a0c2f1a1ab0a7fa11385
Mostafa <mostafa@Mostafas-MacBook-Air.local>
2025-10-11 02:49:03 +0330

    fix: add valuetech-nl.com to remotePatterns for image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "valuetech-nl.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
